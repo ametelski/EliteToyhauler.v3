ec6e8a6f80be99ef500689458b8a99bac58382b8
Adam Metelski <asmetelski@gmail.com>
2019-12-24 10:21:38 -0700

    Upgrade to 3.1

---
+++ EliteToyhauler.v3.Application/Audio/AudioSlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EliteToyhauler.v3.Application.Audio
+{
+    public class AudioSlider
+    {
+        public int Zone { get; set; }
+        
+        public int Min { get; set; }
+        
+        public int Max { get; set; }
+
+        public bool IsMuted { get; set; }
+    }
+}
---
+++ EliteToyhauler.v3.Application/Sensors/Sensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EliteToyhauler.v3.Application.Sensors
+{
+    public class Sensor
+    {
+        public string FriendlyName { get; set; }
+
+        public string SystemName { get; set; }
+
+        public decimal Reading { get; set; }
+    }
+}
